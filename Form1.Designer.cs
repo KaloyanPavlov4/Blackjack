af468ab20b3e4056d422d7b5f6e6ee51eef47774
Kaloyan <kaloyanpavlov4@gmail.com>
2025-01-08 15:10:29 +0200

    Add initial form and card images

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,235 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBoxDealer1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBoxDealer2 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxPlayer2 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxPlayer1 = new System.Windows.Forms.PictureBox();
+            this.buttonStand = new System.Windows.Forms.Button();
+            this.hitMeButton = new System.Windows.Forms.Button();
+            this.dealButton = new System.Windows.Forms.Button();
+            this.pictureBoxPlayer3 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxPlayer4 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxPlayer5 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxDealer5 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxDealer4 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxDealer3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDealer1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDealer2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPlayer2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPlayer1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPlayer3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPlayer4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPlayer5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDealer5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDealer4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDealer3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxDealer1
+            // 
+            this.pictureBoxDealer1.Image = global::Blackjack.Properties.Resources.faceDownCard;
+            this.pictureBoxDealer1.InitialImage = null;
+            this.pictureBoxDealer1.Location = new System.Drawing.Point(167, 24);
+            this.pictureBoxDealer1.Name = "pictureBoxDealer1";
+            this.pictureBoxDealer1.Size = new System.Drawing.Size(71, 96);
+            this.pictureBoxDealer1.TabIndex = 27;
+            this.pictureBoxDealer1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(105, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 26;
+            this.label2.Text = "Dealer";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(110, 195);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 25;
+            this.label1.Text = "Player";
+            // 
+            // pictureBoxDealer2
+            // 
+            this.pictureBoxDealer2.Image = global::Blackjack.Properties.Resources.faceDownCard;
+            this.pictureBoxDealer2.Location = new System.Drawing.Point(257, 24);
+            this.pictureBoxDealer2.Name = "pictureBoxDealer2";
+            this.pictureBoxDealer2.Size = new System.Drawing.Size(71, 96);
+            this.pictureBoxDealer2.TabIndex = 24;
+            this.pictureBoxDealer2.TabStop = false;
+            // 
+            // pictureBoxPlayer2
+            // 
+            this.pictureBoxPlayer2.Image = global::Blackjack.Properties.Resources.faceDownCard;
+            this.pictureBoxPlayer2.Location = new System.Drawing.Point(257, 149);
+            this.pictureBoxPlayer2.Name = "pictureBoxPlayer2";
+            this.pictureBoxPlayer2.Size = new System.Drawing.Size(71, 96);
+            this.pictureBoxPlayer2.TabIndex = 23;
+            this.pictureBoxPlayer2.TabStop = false;
+            // 
+            // pictureBoxPlayer1
+            // 
+            this.pictureBoxPlayer1.Image = global::Blackjack.Properties.Resources.faceDownCard;
+            this.pictureBoxPlayer1.Location = new System.Drawing.Point(167, 149);
+            this.pictureBoxPlayer1.Name = "pictureBoxPlayer1";
+            this.pictureBoxPlayer1.Size = new System.Drawing.Size(71, 96);
+            this.pictureBoxPlayer1.TabIndex = 22;
+            this.pictureBoxPlayer1.TabStop = false;
+            // 
+            // buttonStand
+            // 
+            this.buttonStand.Location = new System.Drawing.Point(257, 286);
+            this.buttonStand.Name = "buttonStand";
+            this.buttonStand.Size = new System.Drawing.Size(75, 23);
+            this.buttonStand.TabIndex = 21;
+            this.buttonStand.Text = "Stand";
+            this.buttonStand.UseVisualStyleBackColor = true;
+            // 
+            // hitMeButton
+            // 
+            this.hitMeButton.Location = new System.Drawing.Point(163, 286);
+            this.hitMeButton.Name = "hitMeButton";
+            this.hitMeButton.Size = new System.Drawing.Size(75, 23);
+            this.hitMeButton.TabIndex = 20;
+            this.hitMeButton.Text = "Deal";
+            this.hitMeButton.UseVisualStyleBackColor = true;
+            // 
+            // dealButton
+            // 
+            this.dealButton.Location = new System.Drawing.Point(14, 247);
+            this.dealButton.Name = "dealButton";
+            this.dealButton.Size = new System.Drawing.Size(75, 23);
+            this.dealButton.TabIndex = 19;
+            this.dealButton.Text = "Start";
+            this.dealButton.UseVisualStyleBackColor = true;
+            // 
+            // pictureBoxPlayer3
+            // 
+            this.pictureBoxPlayer3.Image = global::Blackjack.Properties.Resources.faceDownCard;
+            this.pictureBoxPlayer3.Location = new System.Drawing.Point(342, 149);
+            this.pictureBoxPlayer3.Name = "pictureBoxPlayer3";
+            this.pictureBoxPlayer3.Size = new System.Drawing.Size(71, 96);
+            this.pictureBoxPlayer3.TabIndex = 28;
+            this.pictureBoxPlayer3.TabStop = false;
+            this.pictureBoxPlayer3.Visible = false;
+            // 
+            // pictureBoxPlayer4
+            // 
+            this.pictureBoxPlayer4.Image = global::Blackjack.Properties.Resources.faceDownCard;
+            this.pictureBoxPlayer4.Location = new System.Drawing.Point(427, 149);
+            this.pictureBoxPlayer4.Name = "pictureBoxPlayer4";
+            this.pictureBoxPlayer4.Size = new System.Drawing.Size(71, 96);
+            this.pictureBoxPlayer4.TabIndex = 29;
+            this.pictureBoxPlayer4.TabStop = false;
+            this.pictureBoxPlayer4.Visible = false;
+            // 
+            // pictureBoxPlayer5
+            // 
+            this.pictureBoxPlayer5.Image = global::Blackjack.Properties.Resources.faceDownCard;
+            this.pictureBoxPlayer5.Location = new System.Drawing.Point(510, 149);
+            this.pictureBoxPlayer5.Name = "pictureBoxPlayer5";
+            this.pictureBoxPlayer5.Size = new System.Drawing.Size(71, 96);
+            this.pictureBoxPlayer5.TabIndex = 30;
+            this.pictureBoxPlayer5.TabStop = false;
+            this.pictureBoxPlayer5.Visible = false;
+            // 
+            // pictureBoxDealer5
+            // 
+            this.pictureBoxDealer5.Image = global::Blackjack.Properties.Resources.faceDownCard;
+            this.pictureBoxDealer5.Location = new System.Drawing.Point(510, 24);
+            this.pictureBoxDealer5.Name = "pictureBoxDealer5";
+            this.pictureBoxDealer5.Size = new System.Drawing.Size(71, 96);
+            this.pictureBoxDealer5.TabIndex = 33;
+            this.pictureBoxDealer5.TabStop = false;
+            this.pictureBoxDealer5.Visible = false;
+            // 
+            // pictureBoxDealer4
+            // 
+            this.pictureBoxDealer4.Image = global::Blackjack.Properties.Resources.faceDownCard;
+            this.pictureBoxDealer4.Location = new System.Drawing.Point(427, 24);
+            this.pictureBoxDealer4.Name = "pictureBoxDealer4";
+            this.pictureBoxDealer4.Size = new System.Drawing.Size(71, 96);
+            this.pictureBoxDealer4.TabIndex = 32;
+            this.pictureBoxDealer4.TabStop = false;
+            this.pictureBoxDealer4.Visible = false;
+            // 
+            // pictureBoxDealer3
+            // 
+            this.pictureBoxDealer3.Image = global::Blackjack.Properties.Resources.faceDownCard;
+            this.pictureBoxDealer3.Location = new System.Drawing.Point(342, 24);
+            this.pictureBoxDealer3.Name = "pictureBoxDealer3";
+            this.pictureBoxDealer3.Size = new System.Drawing.Size(71, 96);
+            this.pictureBoxDealer3.TabIndex = 31;
+            this.pictureBoxDealer3.TabStop = false;
+            this.pictureBoxDealer3.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackgroundImage = global::Blackjack.Properties.Resources.background;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(644, 361);
+            this.Controls.Add(this.pictureBoxDealer5);
+            this.Controls.Add(this.pictureBoxDealer4);
+            this.Controls.Add(this.pictureBoxDealer3);
+            this.Controls.Add(this.pictureBoxPlayer5);
+            this.Controls.Add(this.pictureBoxPlayer4);
+            this.Controls.Add(this.pictureBoxPlayer3);
+            this.Controls.Add(this.pictureBoxDealer1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBoxDealer2);
+            this.Controls.Add(this.pictureBoxPlayer2);
+            this.Controls.Add(this.pictureBoxPlayer1);
+            this.Controls.Add(this.buttonStand);
+            this.Controls.Add(this.hitMeButton);
+            this.Controls.Add(this.dealButton);
+            this.DoubleBuffered = true;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(660, 400);
+            this.MinimumSize = new System.Drawing.Size(660, 400);
+            this.Name = "Form1";
+            this.Text = "Blackjack";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDealer1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDealer2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPlayer2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPlayer1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPlayer3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPlayer4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPlayer5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDealer5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDealer4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDealer3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxDealer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBoxDealer2;
+        private System.Windows.Forms.PictureBox pictureBoxPlayer2;
+        private System.Windows.Forms.PictureBox pictureBoxPlayer1;
+        private System.Windows.Forms.Button buttonStand;
+        private System.Windows.Forms.Button hitMeButton;
+        private System.Windows.Forms.Button dealButton;
+        private System.Windows.Forms.PictureBox pictureBoxPlayer3;
+        private System.Windows.Forms.PictureBox pictureBoxPlayer5;
+        private System.Windows.Forms.PictureBox pictureBoxDealer5;
+        private System.Windows.Forms.PictureBox pictureBoxDealer4;
+        private System.Windows.Forms.PictureBox pictureBoxDealer3;
+        private System.Windows.Forms.PictureBox pictureBoxPlayer4;
     }
 }
 
